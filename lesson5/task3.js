7e67e91c3aa4096f87c18c784a556857b710ee96
ImpressiveNijo <igorpetrenk321@gmail.com>
2025-02-11 12:21:36 +0200

    lesson5

---
+++ lesson5/task3.js
@@ -0,0 +1,3 @@
+const x = 6
+for (let y = 1; x *y <= 60; y++)
+console.log(`${x} x ${y} = ${x*y}`);
---
+++ lesson5/task4.js
@@ -0,0 +1,6 @@
+const x = 6;
+let y = 1;
+while (x*y <= 60) {
+    console.log(`${x} x ${y} = ${x*y}`);
+    y++;
+}
