f91e5783a8329cbae84534c38e12c11f7509e6a3
ImpressiveNijo <igorpetrenk321@gmail.com>
2025-02-24 11:06:58 +0200

    lesson8

---
+++ lesson8/task5.js
@@ -0,0 +1,4 @@
+const firstArray = [1, 2];
+const secondArray = [3, 4];
+const concatenatedArray = firstArray.concat(secondArray);
+console.log(concatenatedArray);
---
+++ lesson8/task6.js
@@ -0,0 +1,4 @@
+const numbersList = [1,5,7,3,2,2,7,112,4346,347,2,36]
+const sortedNumbersList = [...numbersList].sort((a,b) => a - b)
+console.log(numbersList);
+console.log(sortedNumbersList)
